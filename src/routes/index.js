7e438be6365c8a002832f7d835bd3587664b4fc4
ljs <lijs@qll-times.com>
2021-09-05 18:34:07 +0800

    feature : 模板引擎

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,31 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  console.log("aaa");
+  console.log("aaa ====");
+  // 返回页面
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    msg : 'this is a msg,您好',
+    isMe : true,
+    blogList : [
+      {
+        id : 1,
+        title : "aaa"
+      },
+      {
+        id : 2,
+        title : "bbb"
+      },
+      {
+        id : 3,
+        title : "ccc"
+      }
+    ]
   })
 })
 
 router.get('/string', async (ctx, next) => {
+  console.log("bbb");
   ctx.body = 'koa2 string'
 })
 
